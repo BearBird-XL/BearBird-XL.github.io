ff7cd95704640ca1d2fbda0f1649dce9ce314951
lei <970308300@qq.com>
2021-10-11 17:17:57 +0800

    fix：修改index.js

--- web/custom/js/index.js
+++ web/custom/js/index.js
@@ -1,1 +1,6 @@
-document.getElementById("span-date").innerText = new Date().toString();
+setInterval(() => {
+    flushTime();
+}, 1000);
+let flushTime = function () {
+    document.getElementById("span-date").innerText = new Date().toString().substr(4, 20);
+}
