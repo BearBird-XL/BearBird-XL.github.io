1a449f5a9d34941d147281ec34a77d5c2700ce96
lei <970308300@qq.com>
2021-10-11 17:23:37 +0800

    fix：修改index.js

--- web/custom/js/index.js
+++ web/custom/js/index.js
@@ -2,5 +2,25 @@ setInterval(() => {
     flushTime();
 }, 1000);
 let flushTime = function () {
-    document.getElementById("span-date").innerText = new Date().toString().substr(4, 20);
+    document.getElementById("span-date").innerText = dateFormat("YYYY-mm-dd HH:MM:SS", new Date());
+}
+
+function dateFormat(fmt, date) {
+    let ret;
+    const opt = {
+        "Y+": date.getFullYear().toString(),        // 年
+        "m+": (date.getMonth() + 1).toString(),     // 月
+        "d+": date.getDate().toString(),            // 日
+        "H+": date.getHours().toString(),           // 时
+        "M+": date.getMinutes().toString(),         // 分
+        "S+": date.getSeconds().toString()          // 秒
+        // 有其他格式化字符需求可以继续添加，必须转化成字符串
+    };
+    for (let k in opt) {
+        ret = new RegExp("(" + k + ")").exec(fmt);
+        if (ret) {
+            fmt = fmt.replace(ret[1], (ret[1].length === 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+        }
+    }
+    return fmt;
 }
